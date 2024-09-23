8bb85929ba7486c32a5a77255a2153b74f711c5c
FE-hallen <63736472+FE-hallen@users.noreply.github.com>
2024-09-23 10:00:41 -0500

    Fixed edge cases for new timestamp parsing

--- java-sdk-core/src/main/java/software/reloadly/sdk/core/internal/adapter/JackSonDateDeserializer.java
+++ java-sdk-core/src/main/java/software/reloadly/sdk/core/internal/adapter/JackSonDateDeserializer.java
@@ -6,18 +6,28 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 
 public class JackSonDateDeserializer extends JsonDeserializer<Date> {
     @Override
     public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         try {
-            return Date.from(
-                    Instant.from(
-                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm[:ss[Z]]")
-                                    .parse(jsonParser.getText())));
+            TemporalAccessor temporalAccessor = DateTimeFormatter.ofPattern("yyyy-MM-dd[ ]['T']HH:mm[:ss][X]")
+                    .parseBest(jsonParser.getText(), OffsetDateTime::from, LocalDateTime::from);
+
+            if (temporalAccessor instanceof OffsetDateTime) {
+                return Date.from(((OffsetDateTime) temporalAccessor).toInstant());
+            } else if (temporalAccessor instanceof LocalDateTime) {
+                return Date.from(((LocalDateTime) temporalAccessor).atZone(ZoneId.of("UTC")).toInstant());
+            } else {
+                return Date.from(Instant.from(temporalAccessor));
+            }
         } catch (DateTimeParseException e) {
             throw new RuntimeException(e);
         }
