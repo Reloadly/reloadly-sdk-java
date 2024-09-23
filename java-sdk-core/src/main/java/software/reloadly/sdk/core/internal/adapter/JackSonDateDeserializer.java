a09bcd844b6bd326f037c38ea4988b3c8b2d1d3d
FE-hallen <63736472+FE-hallen@users.noreply.github.com>
2024-09-23 08:34:54 -0500

    Update JSON deserializer to handle new timestamp formats in promotions

--- java-sdk-core/src/main/java/software/reloadly/sdk/core/internal/adapter/JackSonDateDeserializer.java
+++ java-sdk-core/src/main/java/software/reloadly/sdk/core/internal/adapter/JackSonDateDeserializer.java
@@ -5,16 +5,20 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 
 public class JackSonDateDeserializer extends JsonDeserializer<Date> {
     @Override
     public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         try {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(jsonParser.getText());
-        } catch (ParseException e) {
+            return Date.from(
+                    Instant.from(
+                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm[:ss[Z]]")
+                                    .parse(jsonParser.getText())));
+        } catch (DateTimeParseException e) {
             throw new RuntimeException(e);
         }
     }
